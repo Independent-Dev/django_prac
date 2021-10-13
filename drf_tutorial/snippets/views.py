32661a29a8b96913c11e431eb785d4b305e4303b
Independent-Dev <juju08217@daum.net>
2021-10-13 23:11:57 +0900

    drf tutorial part5

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -23,6 +34,16 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly]
 
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()  # 여기서 get_object()는 어디에서 튀어나온 것이고 어떤 역할을 하는 것인가.
+
+        # Response(snippet.highlighted)를 찍으면 어떻게 페이지가 렌더링되는지 보임. 그런데 왜 이렇게 되는 것인지는 모르겠음. 
+        return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
