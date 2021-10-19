95828242f24dd45fe065e9b79193ca5b04fa38a6
Independent-Dev <juju08217@daum.net>
2021-10-19 23:38:25 +0900

    <Build REST APIs with Django REST Framework and Python> section5 29강까지. 다양한 generic view의 이용(url 패턴 리팩토링 이전)

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('list/', views.WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', views.WatchDetailAV.as_view(), name="movie-detail"),
     path('stream/', views.StreamPlatformListAV.as_view(), name='stream-list'),
-    path('stream/<int:pk>/', views.StreamPlatformDetailAV.as_view(), name='streamplatform-detail')
+    path('stream/<int:pk>/', views.StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+
+    # path('stream/<int:pk>/review/', views.ReviewList.as_view(), name='review-list'),
+    # path('stream/review/<int:pk>/', views.ReviewDetail.as_view(), name='review-detail'),
+
+    path('review/', views.ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/', views.ReviewDetail.as_view(), name='review-detail'),
 ]
